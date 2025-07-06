bfad0c1beaf0e7830ebf7371c16b610e463e93d6
whatisaProCoder <debnathpritam2005@gmail.com>
2025-07-07 01:10:31 +0530

    init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./styles.css";
 import loadInitialPage from "./js/initial-page-load.js";
 import headerHandler from "./js/handle-header.js";
 
+// load initial page
 loadInitialPage();
 
+// handle header behavior
 headerHandler();
 
 console.log("Project initialised!");
